2b451b4ebdc74526e83a34a402651aa43fd61868
Jessica Van Brummelen <jess.vanbrummelen@gmail.com>
2019-04-02 08:30:10 -0400

    Get byte data / Test playing stream

--- record-and-stream/index.js
+++ record-and-stream/index.js
@@ -2,6 +2,7 @@
 
 // Stream to server:
 // https://subvisual.co/blog/posts/39-tutorial-html-audio-capture-streaming-to-node-js-no-browser-extensions/
+// from google (newer): https://developers.google.com/web/fundamentals/media/recording-audio/
 
 
 let currentStream = null;
@@ -10,17 +11,28 @@ function streamOrStopAudio() {
     if (!currentStream) {
         // start streaming
         let handleSuccess = function (stream) {
+            let bufferSize = 2048;
+
             let context = new AudioContext();
             let source = context.createMediaStreamSource(stream);
-            let processor = context.createScriptProcessor(1024, 1, 1);
+            let processor = context.createScriptProcessor(bufferSize, 1, 1);
 
             source.connect(processor);
             processor.connect(context.destination);
+
+            // todo del:
+            if (window.URL) {
+                player.srcObject = stream;
+            } else {
+                player.src = stream;
+            }
 
             processor.onaudioprocess = function (e) {
                 // Do something with the data, i.e Convert this to WAV
                 // console.log(e.inputBuffer);
                 console.log("still running");
+                let left = e.inputBuffer.getChannelData(0);
+                console.log(left);
             };
 
             console.log(source);
