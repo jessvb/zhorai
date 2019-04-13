493f595337f03fcfb965b55c0255f61cacbca50b
Galit <glukin@mit.edu>
2019-04-12 22:47:16 -0400

    adding dictionary for output of parser

---
+++ semantic-parser/buildDict.py
@@ -0,0 +1,86 @@
+import xml.etree.ElementTree as ET
+
+ecosystems = ["forest", "desert", "rainforest", "grassland", "tundra", "plain"]
+animals = ["camel"]
+negatives = ["n't","not","no","little","small","few"]
+
+
+def combine(id, structureData):
+    if len(structureData[id]) == 2:
+        return structureData[id]
+    else:
+        children = structureData[id].split(' ')
+        sentenceStruct = []
+        for c in children:
+            sentenceStruct.append(combine(c, structureData))
+    return sentenceStruct
+
+def extractWords(part):
+    if type(part) is tuple:
+        word = part[0]
+        pos = part[1]
+        if pos == 'NN' or pos == 'JJ' or pos == 'RB'or pos == 'NNS':
+            if (word in ecosystems) or (word in animals):
+                return ('subject',word)
+            else:
+                if word in negatives:
+                    return ('neg',word)
+                else:
+                    return ('pos',word)
+        return
+    else:
+        words = []
+        for p in part:
+            res = extractWords(p)
+            if type(res) is tuple:
+                words = words + [res]
+            elif res:
+                words = words + res
+    return words
+
+tree = ET.parse('example/sentences.xml')
+root = tree.getroot()
+
+for doc in root:
+    for sentences in doc:
+        s = []
+        for sentence in sentences:
+            structureData = {}
+            ccg = sentence[1]
+            subRoot = ccg.attrib['root']
+            for span in ccg:
+                if span.attrib['pos'] == "None":
+                    structureData[span.attrib['id']] = (span.attrib['child'])
+                else:
+                    structureData[span.attrib['id']] = (span.attrib['base'], span.attrib['pos'])
+            s.append(combine(subRoot, structureData))
+
+words = []
+for sentence in s:
+    words.append(extractWords(sentence))
+
+res = {}
+for wList in words:
+    subjects = [subject[1] for subject in wList if subject[0] == 'subject']
+    isNeg = False
+    for corr, w in wList:
+        if corr == 'subject':
+            continue
+        if corr == 'neg':
+            isNeg = True
+            continue
+        if isNeg == True:
+            for i in subjects:
+                if i in res.keys():
+                    res[i].append(('neg',w))
+                else:
+                    res[i] = [('neg',w)]
+            isNeg = False
+        else:
+            for i in subjects:
+                if i in res.keys():
+                    res[i].append(('pos',w))
+                else:
+                    res[i] = [('pos',w)]
+
+print(res)
--- semantic-parser/parse.sh
+++ semantic-parser/parse.sh
@@ -1,3 +1,6 @@
+# script receives two arguments
+# 1. the input file (inculuding its relative path)
+# 2. the output relative path
 infile=$1
 outpath=$2
 
@@ -5,5 +8,6 @@ cat $infile | sed -f ccg2lambda/en/tokenizer.sed > $outpath/sentences.tok
 ccg2lambda/candc-1.00/bin/candc --models ccg2lambda/candc-1.00/models --candc-printer xml --input $outpath/sentences.tok > $outpath/sentences.candc.xml
 python ccg2lambda/en/candc2transccg.py $outpath/sentences.candc.xml > $outpath/sentences.xml
 python ccg2lambda/scripts/semparse.py $outpath/sentences.xml ccg2lambda/en/semantic_templates_en_emnlp2015.yaml $outpath/sentences.sem.xml
-python ccg2lambda/scripts/prove.py $outpath/sentences.sem.xml --graph_out $outpath/graphdebug.html
+#python ccg2lambda/scripts/prove.py $outpath/sentences.sem.xml --graph_out $outpath/graphdebug.html
 python ccg2lambda/scripts/visualize.py $outpath/sentences.xml > $outpath/sentences.html
+python buildDict.py > $outpath/dictionary.txt
