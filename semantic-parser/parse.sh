013a47e73b2ac57d9a2fdf135793d1a668f948d4
Galit <glukin@mit.edu>
2019-04-14 08:52:41 -0400

    documentation

--- semantic-parser/parse.sh
+++ semantic-parser/parse.sh
@@ -4,11 +4,18 @@
 infile=$1
 outpath=$2
 
+#splitting sentences so that each sentence appears in new line
 python prepareCorpus.py $infile > $outpath/sentences.txt
+#splitting words up into tokens
 cat $outpath/sentences.txt | sed -f ccg2lambda/en/tokenizer.sed > $outpath/sentences.tok
+#label tokens
 ccg2lambda/candc-1.00/bin/candc --models ccg2lambda/candc-1.00/models --candc-printer xml --input $outpath/sentences.tok > $outpath/sentences.candc.xml
+#parse sentence besed on labels
 python ccg2lambda/en/candc2transccg.py $outpath/sentences.candc.xml > $outpath/sentences.xml
-python ccg2lambda/scripts/semparse.py $outpath/sentences.xml ccg2lambda/en/semantic_templates_en_emnlp2015.yaml $outpath/sentences.sem.xml
+#don't need the lambda expressions for now
+#python ccg2lambda/scripts/semparse.py $outpath/sentences.xml ccg2lambda/en/semantic_templates_en_emnlp2015.yaml $outpath/sentences.sem.xml
 #python ccg2lambda/scripts/prove.py $outpath/sentences.sem.xml --graph_out $outpath/graphdebug.html
+#create visualization that can be shown in website
 python ccg2lambda/scripts/visualize.py $outpath/sentences.xml > $outpath/sentences.html
+#build dictionary output for word embedder
 python buildDict.py > $outpath/dictionary.txt
