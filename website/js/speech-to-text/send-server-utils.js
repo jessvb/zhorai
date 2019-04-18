a1364a8d78d1e89c17c7657fe175ddcceba34bc0
Jessica Van Brummelen <jess.vanbrummelen@gmail.com>
2019-04-17 23:10:43 -0400

    Add websocket connection to node.js

---
+++ website/js/speech-to-text/send-server-utils.js
@@ -0,0 +1,26 @@
+/* --- utils for sending text to the server --- */
+var url = 'ws://localhost:8080';
+
+function sendText(text) {
+    sendJson({
+        'text': text
+    });
+}
+
+function sendJson(json) {
+    var socket = new WebSocket(url);
+    socket.onopen = function (event) {
+        socket.send(JSON.stringify(json));
+        socket.close();
+    };
+}
+
+function makeTextFile() {
+    var socket = new WebSocket(url);
+    socket.onopen = function (event) {
+        socket.send(JSON.stringify({
+            'command': 'makeTextFile'
+        }));
+        socket.close();
+    };
+}
