1b3c8206a6fba55946ccfe87db874b883c44abca
ifueko <ifueko@mit.edu>
2019-04-24 10:42:59 -0400

    updated corpus cleaning to separate hyphenated words like bert

--- word-embeddings/clean_corpus.py
+++ word-embeddings/clean_corpus.py
@@ -3,7 +3,7 @@ import re
 def filterfun(val):
 	return val is not '' and val is not ' '
 
-with open('embedding_corpus_raw.csv', 'r') as in_file, open ('embedding_corpus.csv', 'w') as out_file:
+with open('embedding_corpus_raw.csv', 'r') as in_file, open ('embedding_corpus.txt', 'w') as out_file:
 	seen = set()
 	for line in in_file:
 		if line in seen: continue
@@ -11,6 +11,7 @@ with open('embedding_corpus_raw.csv', 'r') as in_file, open ('embedding_corpus.c
 		line = re.sub(' +', ' ', line)
 		line = re.sub(',+', '', line)
 		line = re.sub('\n', '', line)
+		line = re.sub('-', ' ', line)
 		lines = [re.sub('"', '', item) for item in list(filter(filterfun, line.split(".")))]
 		for sentence in lines: 
-			if sentence is not ' ' and sentence is not '\n' and len(sentence) > 1: out_file.write(sentence.strip()+"\n")
+			if sentence is not ' ' and sentence is not '\n' and len(sentence.split()) > 3: out_file.write(sentence.strip()+"\n")
--- word-embeddings/embedding.py
+++
@@ -1,8 +0,0 @@
-from bert_embedding import BertEmbedding
-
-bert = BertEmbedding()
-
-words = "forests are more wet than deserts, which are dry."
-
-embedding = bert(words)
-print(embedding[0])
