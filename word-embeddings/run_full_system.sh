7e443245beda3ce6b7c664d86318a14147496bf7
Ubuntu <ubuntu@ip-172-31-23-250.us-east-2.compute.internal>
2019-05-01 17:57:07 +0000

    adding requirements, system script and updated embedding dict

--- word-embeddings/run_full_system.sh
+++ word-embeddings/run_full_system.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python train.py --corpus-file embedding_corpus.txt --checkpoint-prefix model_initial --save-frequency 100 --display-frequency 25 --epochs 100
+python train.py --corpus-file embedding_corpus.txt --checkpoint-prefix model_initial --save-frequency 100 --display-frequency 25 --epochs 100 --save-embedding-dict
 
 python train.py --corpus-file ecosystem-sentences.txt --checkpoint-prefix model_eco --save-frequency 100 --display-frequency 25 --epochs 100 --model-checkpoint model_initial-0100.tar
 
