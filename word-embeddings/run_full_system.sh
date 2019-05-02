d15ce688593ef7ca5dabdc526e54a98ab0f71e0d
Ubuntu <ubuntu@ip-172-31-23-250.us-east-2.compute.internal>
2019-05-02 02:39:01 +0000

    k-fold validation results

--- word-embeddings/run_full_system.sh
+++ word-embeddings/run_full_system.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python train.py --corpus-file embedding_corpus.txt --checkpoint-prefix model_initial --save-frequency 100 --display-frequency 25 --epochs 100 --load-embedding-from-file
+python train.py --corpus-file embedding_corpus.txt --checkpoint-prefix model_initial --save-frequency 5 --display-frequency 5 --epochs 20 --load-embedding-from-file
 
-python train.py --corpus-file ecosystem-sentences.txt --checkpoint-prefix model_eco --save-frequency 5 --display-frequency 25 --epochs 110 --model-checkpoint results/model_initial-0100.tar --results-dir results_eco
+python train.py --corpus-file ecosystem-sentences.txt --checkpoint-prefix model_eco --save-frequency 5 --display-frequency 5 --epochs 30 --model-checkpoint results/model_initial-0020.tar --results-dir results_eco
 
-python visualize_embedding_results.py
+#python visualize_embedding_results.py
--- word-embeddings/train_k_fold_validation.py
+++ word-embeddings/train_k_fold_validation.py
@@ -30,39 +30,41 @@ if torch.cuda.is_available():
 	torch.cuda.manual_seed(np.random.randint(1, 10000))
 	torch.backends.cudnn.enabled = True 
 args.classes = ["desert", "rainforest", "grassland", "tundra", "ocean"]
-model = EmbeddingModel(len(args.classes), args.embedding_type)
-if torch.cuda.is_available():
-	model = model.cuda()
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
-model = model.train()
-starting_epoch = 0
-running_loss = 0.0
-if len(args.model_checkpoint) > 0:
-	checkpoint = torch.load(args.model_checkpoint)
-	model.load_state_dict(checkpoint['model_state_dict'])
-	optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-	starting_epoch = checkpoint['epoch']
-	running_loss = checkpoint['running_loss']
 train_set, train_labels, _, __, ___ = generateData(args.corpus_file, args.classes, 1.0, args.load_embedding_from_file, args.save_embedding_dict)
-num_examples = len(train_set) / args.k
+num_examples = int(np.ceil(len(train_set) / args.k))
 folds = [(train_set[i:i+num_examples], train_labels[i:i+num_examples]) for i in range(0, len(train_set), num_examples)]
 print('Performing K-Fold Cross Validation...')
-print('Starting from epoch %d' % (starting_epoch + 1))
-for i in len(folds):
+acc = []
+for i in range(len(folds)):
+	train_acc = 0.0
+	test_acc = 0.0
+	model = EmbeddingModel(len(args.classes), args.embedding_type)
+	if torch.cuda.is_available():
+		model = model.cuda()
+	criterion = torch.nn.CrossEntropyLoss()
+	optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+	model = model.train()
+	running_loss = 0.0
+	starting_epoch = 0
+	if len(args.model_checkpoint) > 0:
+		checkpoint = torch.load(args.model_checkpoint)
+		model.load_state_dict(checkpoint['model_state_dict'])
+		optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+		starting_epoch = checkpoint['epoch']
+		running_loss = checkpoint['running_loss']
 	curr_results_dir = os.path.join(args.results_dir,"results_fold_{0}".format(i))
 	os.mkdir(curr_results_dir)
-	print("Leaving out fold #{0}".format(i+1)
+	print("Leaving out fold #{0}".format(i+1))
 	curr_test_set, curr_test_labels = folds[i]
 	curr_train_set = []
 	curr_train_labels = []
-	for j in len(folds):
+	for j in range(len(folds)):
 		if i != j:
-			curr_train_set = curr_train_set + folds[j]
-			curr_train_labels = curr_train_labels + folds[j]
+			curr_train_set = curr_train_set + folds[j][0]
+			curr_train_labels = curr_train_labels + folds[j][1]
 	for epoch in progressbar(range(starting_epoch, args.epochs)):
 		for i in range(len(curr_train_set)):
-			inputs = curr_set[i]
+			inputs = curr_train_set[i]
 			labels = curr_train_labels[i]
 			if torch.cuda.is_available():
 				inputs = inputs.cuda()
@@ -98,6 +100,7 @@ for i in len(folds):
 				_, predicted = torch.max(outputs.data, 1)
 				total += labels.size(0)
 				correct += (predicted == labels).sum().item()
+			train_acc = correct/total
 			print('\tTrain accuracy = %.3f%% ' % (100.0 * correct/total))
 			correct = 0.0
 			total = 0.0
@@ -111,6 +114,22 @@ for i in len(folds):
 				_, predicted = torch.max(outputs.data, 1)
 				total += labels.size(0)
 				correct += (predicted == labels).sum().item()
+			test_acc = correct/total
 			print('\tTest accuracy = %.3f%% ' % (100.0 * correct/total))
 			model = model.train()
+	acc.append((train_acc, test_acc))
 print('Training Complete.')
+tr_avg = 0.0
+te_avg = 0.0
+for i in range(len(acc)):
+	tr, te = acc[i]
+	print("Fold {0}: Train Acc: {1}, Test Acc: {2}".format(i, tr, te))
+	tr_avg += tr
+	te_avg += te
+
+tr_avg /= len(acc)
+te_avg /= len(acc)
+
+print("Average train accuracy: {0}".format(tr_avg))
+print("Average test accuracy: {0}".format(te_avg))
+
