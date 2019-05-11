0ca07ce687e9fa352c03be55a70a6cda7507a2a1
Ubuntu <ubuntu@ip-172-31-23-250.us-east-2.compute.internal>
2019-05-11 00:27:14 +0000

    updating model and k-fold validation

--- word-embeddings/model.py
+++ word-embeddings/model.py
@@ -46,10 +46,9 @@ class ConvolutionalEmbeddingModel(nn.Module):
 	def __init__(self, num_classes):
 		super(ConvolutionalEmbeddingModel, self).__init__()
 		self.lstm1 = SimpleLSTM(768, 512, False)
-		self.lstm2 = SimpleLSTM(512, 256, True)
-		self.conv1 = nn.Conv1d(1, 5, 65)
-		self.conv2 = nn.Conv1d(5, 1, 65)
-		self.l2 = nn.Linear(128, num_classes)
+		self.lstm2 = SimpleLSTM(512, 384, True)
+		self.conv1 = nn.Conv1d(1, 1, 129)
+		self.l2 = nn.Linear(256, num_classes)
 
 	def forward(self, x):
 		x = self.embedding(x)
@@ -61,6 +60,5 @@ class ConvolutionalEmbeddingModel(nn.Module):
 		x = self.lstm2(x)
 		x = x.unsqueeze(0)
 		x = F.relu(self.conv1(x))
-		x = F.relu(self.conv2(x))
 		x = x.squeeze(0)
 		return x
--- word-embeddings/run_k_fold_validation.sh
+++ word-embeddings/run_k_fold_validation.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-python -u train_k_fold_validation.py --checkpoint-prefix model_conv --save-frequency 5 --display-frequency 5 --load-embedding-dict-from-file --embedding-type conv | tee k-fold-validation.txt
-python -u train_k_fold_validation.py --checkpoint-prefix model_linear --save-frequency 5 --display-frequency 5 --load-embedding-dict-from-file --embedding-type linear | tee k-fold-validation.txt
-
-#python visualize_embedding_results.py
+python -u train_k_fold_validation.py --results-dir results_k_fold_conv --checkpoint-prefix model_conv --save-frequency 5 --display-frequency 5 --load-embedding-dict-from-file --embedding-type conv | tee results_k_fold_conv/conv-k-fold-validation.txt
+python -u train_k_fold_validation.py --results-dir results_k_fold_linear --checkpoint-prefix model_linear --save-frequency 5 --display-frequency 5 --load-embedding-dict-from-file --embedding-type linear | tee results_k_fold_linear/linear-k-fold-validation.txt
