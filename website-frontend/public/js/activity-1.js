8d84fdb8d6de0c95c4e27fd98f706ba2cb306585
Jessica Van Brummelen <jess.vanbrummelen@gmail.com>
2021-02-17 14:17:05 -0800

    [WIP] clean animal data

--- website-frontend/public/js/activity-1.js
+++ website-frontend/public/js/activity-1.js
@@ -10,18 +10,23 @@ var currBtnIsMic = true;
 var sentences = {
     fox: [
         "Red foxes are solitary hunters",
-        "Foxes feed on rodents, rabbits, birds, and other small game",
+        "Foxes like to eat rabbits",
+        // "Foxes like to eat birds and other small game",
         "Foxes have a flexible diet",
-        "Foxes will eat fruit and vegetables, fish, frogs, and even worms",
+        "Foxes will eat fruits and vegetables",
+        "Foxes will eat fish and frogs",
+        "Foxes will even eat worms",//, fish, frogs, and worms",
         "If living among humans, foxes will opportunistically dine on garbage and pet food",
         "Like a cat's, the fox's thick tail aids its balance, but it has other uses as well",
-        "A fox uses its tail (or “brush”) as a warm cover in cold weather and as a signal flag to communicate with other foxes",
-        "Foxes also signal each other by making scent posts—urinating on trees or rocks to announce their presence",
+        "A fox uses its tail as a warm cover in cold weather",
+        "Foxes use their tails as a signal flag to communicate with other foxes",
+        "Foxes also signal each other by making scent posts and urinating on trees or rocks to announce their presence",
         "Foxes are omnivorous mammals that are light on their feet",
         "Foxes are often mistaken for other members of the Canidae family, which include jackals, wolves and dogs",
         "Foxes stand out from their relatives because of their long, thin legs, lithe frame, pointed nose and bushy tail",
         "These animals are very social and live flexible lives",
-        "Foxes are found all over the world — in North America, Europe, Asia and North Africa — and call a wide range of terrains their home",
+        "Foxes are found all over the world, including North America and Africa",//, Europe, Asia
+        "Foxes call a wide range of terrains their home",
         "Foxes also eat a greatly varied diet",
         "Foxes are usually monogamous",
         "This means that foxes have only one mate for life",
@@ -32,20 +37,21 @@ var sentences = {
         "Foxes can identify each other's voices, just like humans",
         "The red fox has different sounds foxes use to communicate",
         "These vocalizations include yips, growls and howls",
-        "The small, slender body of a red fox allows it to run nearly",
-        "Fox hunting was a popular recreation sport in England since",
-        "Hunting foxes without the aid of dogs is still practiced in the United Kingdom and several other countries including the United States",
+        "The small, slender body of a red fox allows it to run quickly",
+        "Fox hunting was a popular recreation sport in England",
+        // "Hunting foxes without the aid of dogs is still practiced in the United Kingdom and other places",
         "In folklore, foxes are typically characterized as cunning creatures sometimes having magical powers",
         "In the wild, fox cubs can fall prey to eagles",
-        "Coyotes, gray wolves, bears and mountain lions are all predators for adult foxes",
+        // "Coyotes, gray wolves, bears and mountain lions are all predators for adult foxes",
+        // "Foxes have predators, like wolves and mountain lions",
         "Foxes have excellent hearing",
         "Foxes can hear low-frequency sounds and rodents digging underground",
     ],
     butterfly: [
         "Butterflies are insects",
-        "Butterflies go through a four-step process called metamorphosis – from egg, to caterpillar, to chrysalis to a butterfly",
+        "Butterflies go through metamorphosis, which happens in four stages", //"from egg, to caterpillar, to chrysalis to a butterfly",
         "Butterflies are insects, but large scaly wings set butterflies apart",
-        "These wings allow butterflies to fly but only when their body temperature is above 86 degrees",
+        "These wings allow butterflies to fly but only when their temperature is above 86 degrees",
         "The fastest butterflies can fly up to 30 miles per hour",
         "Scientists estimate that there are 28,000 species of butterflies throughout the world",
         "Most butterflies are found in tropical rainforests, but butterflies can live in all climates and altitudes of the world",
@@ -53,7 +59,7 @@ var sentences = {
         "Many believe butterflies got their name because butterflies would fly around the buckets of milk on farms",
         "While the milk was being churned into butter, many noticed these flying insects would appear and soon butterflies were being called butterflies",
         "Butterflies have three body parts, like all other insects: the head, the thorax (chest), and the abdomen (bottom)",
-        "The butterfly’s four wings and six legs are attached to the thorax",
+        "Butterflies have four wings and six legs",// are attached to the thorax",
         "Butterflies are colorful for many reasons",
         "The colors help butterflies attract a mate and absorb heat and the color also helps butterflies blend in among the flowers when butterflies are feeding",
         "Butterflies change four times during their lives in a process which is called metamorphosis",
@@ -66,15 +72,17 @@ var sentences = {
         "Finally, the chrysalis breaks open and a butterfly comes out (stage 4)",
         "Now the adult butterfly will begin the process all over again by laying eggs of its own",
         "Most butterflies live on the nectar and pollen from flowers",
-        "Butterflies have a long, flexible, tube-like tongue that goes down into the nectar of the flower",
+        "Butterflies have a long, tube-like tongue that goes down into the nectar of the flower",
         "Butterflies suck the nectar up through this tongue",
-        "Female butterflies are usually larger than males and butterflies live longer",
+        "Female butterflies are usually larger than males",
         "Butterflies can live anywhere from 2 days to 11 months",
     ],
     bear: [
-        "The awe-inspiring brown bear lives in the forests and mountains of northern North America, Europe, and Asia",
+        "The brown bear often lives in the mountains",
+        "Bears are impressive mammals",
+        "Bears live in many places, including North America and Asia",
         "The brown bear is the most widely distributed bear in the world",
-        "The world's largest brown bears are found in coastal British Columbia and Alaska, and on islands such as Kodiak",
+        "The largest brown bears are in British Columbia and Alaska",//, and Kodiak Island",
         "Bears hibernate, which means they sleep for long periods of time during the winter",
         "What bears look like: The American Black Bear is the most common bear native to North America",
         "The coat of a Black Bear is shaggy and usually black but it can also be dark brown, cinnamon, or yellow-brown",
@@ -90,38 +98,39 @@ var sentences = {
         "During the winter bears hibernate in dens",
         "What bears eat: American Black Bears are omnivores",
         "Most bears' diet is plant based",
-        "American Black Bears mainly feed on vegetation including herbs, grasses, roots, buds, shoots, honey, nuts, fruit, berries and seeds",
-        "Bears will also eat fish, small mammals, insects, dead animals and garbage",
+        "American Black Bears mainly feed on vegetation including herbs and seeds", // , grasses, roots, buds, shoots, honey, nuts, fruit, berries
+        "Bears will also eat fish, dead animals, insects and garbage", // , small mammals, insects,
         "In northern regions, black bears eat salmon that bears catch in a stream",
         "Behavior: Most bears become active a half-hour before sunrise, take a nap or two during the day, and bed down for the night an hour or two after sunset",
         "Some bears are active mainly at night to avoid people or other bears",
     ],
     frog: [
-        "Frogs are small animals that can jump very well",
+        "Frogs are small animals",
+        "Frogs can jump very well",
         "Frogs are similar to toads",
-        "However, a frog has smooth skin and long legs",
-        "A toad has rough skin and shorter legs",
+        "Frogs have smooth skin and long legs",
+        // "A toad has rough skin and shorter legs",
         "Frogs are found throughout the world except in very cold places",
         "Frogs are most common in rain forests",
         "Frogs are amphibians, meaning that frogs can live in water or on land",
         "Most frogs spend most of their lives in water",
         "Some live in underground holes or in trees",
         "A frog has smooth, moist skin and big, bulging eyes",
-        "Its hind legs are more than twice as long as its front ones",
+        "Frogs' hind legs are more than twice as long as its front ones",
         "Most frogs have webbed back feet to help frogs leap and swim",
         "Tree frogs have sticky disks on the tips of their fingers and toes",
         "These disks help frogs climb slippery surfaces",
         "Many frogs are tiny",
         "Frogs can be less than an inch (5 centimeters) long",
         "The largest frogs are about a foot (30 centimeters) long",
-        "Most frogs are green, brown, gray, or yellow",
-        "Some are brightly colored",
+        "Many frogs are green, brown or yellow", //, gray 
+        "Some frogs are brightly colored",
         "A frog catches prey by flicking out its long, sticky tongue",
         "Most frogs eat insects and worms",
-        "Some also eat other frogs, rodents, and reptiles",
+        "Some frogs also eat rodents and reptiles", //other frogs,
         "Frogs have glands in their skin that make poison",
-        "But this poison does not protect frogs from snakes, birds, and other enemies",
-        "Instead, frogs most often protect themselves by blending in with their surroundings",
+        "Frogs' poison does not protect them from snakes and birds", //, and other enemies
+        "Frogs most often protect themselves by blending in with their surroundings",
         "Frogs usually lay their eggs in water",
         "Frogs can lay hundreds or thousands of eggs",
         "Within a few weeks, frog eggs hatch into tadpoles",
@@ -136,35 +145,42 @@ var sentences = {
         "There is only one species of domestic horse, but around 400 different breeds that specialize in everything from pulling wagons to racing",
         "All horses are grazers",
         "While most horses are domestic, others remain wild",
-        "Feral horses are the descendents of once-tame animals that have run free for generations",
-        "Groups of such horses can be found in many places around the world",
+        "Feral horses are the descendents of previously tame animals that have run free for generations",
+        "Groups of feral horses can be found in many places around the world",
         "Free-roaming North American mustangs, for example, are the descendents of horses brought by Europeans more than 400 years ago",
         "Wild horses generally gather in groups of 3 to 20 animals",
         "A stallion (mature male) leads the group, which consists of mares (females) and young foals",
         "When young male horses become colts, at around two years of age, the stallion drives them away",
         "The colts then roam with other young male horses until they can gather their own band of females",
-        "The Przewalski's horse is the only truly wild horse whose ancestors were never domesticated",
-        "Ironically, this stocky, sturdy animal exists today only in captivity",
-        "The last wild Przewalski's horse was seen in Mongolia in 1968",
+        // "The Przewalski's horse is the only truly wild horse whose ancestors were never domesticated",
+        // "Ironically, this stocky, sturdy animal exists today only in captivity",
+        // "The last wild Przewalski's horse was seen in Mongolia in 1968",
         "Horses are social mammals",
         "In wild or feral populations, horses form herds with a social hierarchy",
-        "These herds, can have up to 26 mares, 5 stallions, and various ages of young",
-        "Horse herds have a well-established social hierarchy, with alpha males being dominant and spending the majority of their time defending the herd from predators or competing males",
+        "Horse herds, can have up to 26 mares, 5 stallions, and various ages of young",
+        "Horse herds have a social hierarchy, with alpha males being dominant and spending the majority of their time defending the herd from predators or competing males",
         "Horses are active at different times of the day, depending on the season",
-        "In hot weather, horses graze in morning or evenings to avoid mid-day high temperatures",
+        "In hot weather, horses graze in morning or evenings to avoid midday high temperatures",
         "Horses sleep in segments throughout the day, which are usually not more than two hours long",
         "Horses also avoid laying down for more than an hour at a time if possible, and can be seen sleeping while standing up",
-        "In horses, the nostrils, muzzle, whiskers, and cheeks all have whiskers that are used to perceive the environment through touch",
+        "Horses' nostrils, muzzle and cheeks have whiskers",
+        "Horses' whiskers are used to perceive the environment through touch", //whiskers, 
         "Vision is the primary means of perceiving the environment in horses",
-        "Ears are long, slender, and upright, which aid in auditory perception",
-        "Although horses' sense of smell is important, it is not the chief means of perception and provides a smaller role than vision or the sensitive receptors on the nostrils, muzzle, whiskers, or cheeks",
+        "Horses' ears are long and slender, which aids in auditory perception", //and upright,
+        "Although horses' sense of smell is important, it is not the chief means of perception",
+        // "and provides a smaller role than vision or the sensitive receptors on the nostrils, muzzle, whiskers, or cheeks",
         "Horses communicate with each other mainly through facial gestures and vocalizations",
-        "Grunting, biting, shoving, and kicking may occur among horses to establish or reinforce the hierarchy structure and express dominance",
+        "Grunting, biting and kicking may occur among horses to express dominance", //, shoving, establish or reinforce the hierarchy structure and
         "Horses have an array of facial gestures",
         "Positive horse reactions include raising of the lips to expose upper teeth, similar to a smile, and head bobbing or pointing the ears forward and erect",
         "Aggressive horse facial gestures include the ears being laid back and the nostrils closed while exposing the same teeth",
     ],
     beaver: ['TODO'],
+    vanilla: ['TODO'],
+    chocolate: ['TODO'],
+    strawberry: ['TODO'],
+    cookiesncream: ['TODO'],
+    mintchocolatechip: ['TODO'],
 };
 
 /* -------------- Initialize functions -------------- */
@@ -263,19 +279,19 @@ function afterRecording(recordedText) {
         categorySentences = sentences.butterfly;
         saidKnownCategory = true;
     } else if (recordedText.toLowerCase().includes('bear')) {
-        category = 'bear';
+        category = 'bears';
         categorySentences = sentences.bear;
         saidKnownCategory = true;
     } else if (recordedText.toLowerCase().includes('frog')) {
-        category = 'frog';
+        category = 'frogs';
         categorySentences = sentences.frog;
         saidKnownCategory = true;
     } else if (recordedText.toLowerCase().includes('horse')) {
-        category = 'horse';
+        category = 'horses';
         categorySentences = sentences.horse;
         saidKnownCategory = true;
     } else if (recordedText.toLowerCase().includes('beaver')) {
-        category = 'beaver';
+        category = 'beavers';
         categorySentences = sentences.beaver;
         saidKnownCategory = true;
     }
